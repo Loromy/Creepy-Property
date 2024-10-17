317596dd8f8bdd50197ef7d6f63ff719b1904202
Loromy <sacul.koehler@gmail.com>
2024-10-17 17:18:43 +0200

    V0.0.1.1 Nur das Fenster

--- src/main/java/com/example/thecreepyproparty/entity/Player.java
+++ src/main/java/com/example/thecreepyproparty/entity/Player.java
@@ -3,7 +3,6 @@ package com.example.thecreepyproparty.entity;
 import javafx.application.Application;
 import javafx.stage.Stage;
 
-import java.awt.*;
 
 public class Player extends Application {
     public Player() {
