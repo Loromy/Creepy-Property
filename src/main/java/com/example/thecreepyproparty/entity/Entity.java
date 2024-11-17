477de1d3406cc22c7447913cd10817333d362d3b
Loromy <sacul.koehler@gmail.com>
2024-11-17 16:46:01 +0100

    V0.0.2.2.0 player texture

---
+++ src/main/java/com/example/thecreepyproparty/entity/Entity.java
@@ -0,0 +1,7 @@
+package com.example.thecreepyproparty.entity;
+
+import javafx.scene.image.Image;
+
+public class Entity {
+    public Image up1, up2, up3, up4;
+}
--- src/main/java/com/example/thecreepyproparty/entity/Player.java
+++ src/main/java/com/example/thecreepyproparty/entity/Player.java
@@ -1,30 +1,39 @@
 package com.example.thecreepyproparty.entity;
 
 
+import javafx.scene.image.Image;
 import javafx.scene.shape.Rectangle;
 import javafx.scene.paint.Color;
 
+import javax.imageio.ImageIO;
+import java.io.File;
+import java.io.IOException;
 
 
-
-public class Player {
-    private Rectangle r_player;
+public class Player extends Entity{
+    private final Rectangle r_player;
     private double strgSpeed = 0; // speed if strg pressed
-    private double speed = 3; // speed (standard 3)
+    private final double speed = 3; // speed (standard 3)
     private double x = 50; // position x
     private double y = 50; // position y
 
 
     public Player()  {
-        r_player = new Rectangle(x, y, 64, 64);
-        r_player.setFill(Color.DARKRED);
-        r_player.setStroke(Color.RED);
-        r_player.setStrokeWidth(3);
+        this.r_player = new Rectangle(x, y, 64, 64);
+
+        setDefaultValues();
+        getPlayerImage();
+    }
+
+    // set methode
+    public void setDefaultValues(){
+        this.r_player.setFill(Color.DARKRED);
+        this.r_player.setStroke(Color.RED);
+        this.r_player.setStrokeWidth(3);
 
         System.out.println("[System]: Player created!");
     }
 
-    // set methode
     public void setX(double x){
         this.x = x;
         r_player.setX(x); // update player
@@ -44,6 +53,15 @@ public class Player {
     }
 
     // get methode
+    public void getPlayerImage(){
+        try {
+            up1 = ;
+            //images player
+        } catch (IOException e) {
+            System.out.println("[ERROR]: Player texture can not be loaded!");
+        }
+    }
+
     public Rectangle getPlayer(){
         return r_player;
     }
