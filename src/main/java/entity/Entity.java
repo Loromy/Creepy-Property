c3ca7967cbc35480879b6614116d2cf7b6bff568
Loromy <sacul.koehler@gmail.com>
2024-11-19 12:15:30 +0100

    V0.0.2.1.7 Buck-fix Fahler in der console

--- src/main/java/entity/Entity.java
+++
@@ -1,7 +0,0 @@
-package entity;
-
-import javafx.scene.image.Image;
-
-public class Entity {
-    public Image up1, up2, up3, up4, down1, down2, down3, down4, left1, left2, left3, left4, right1, right2, right3, right4;
-}
--- src/main/java/theCreepyProperty/main/Main.java
+++ src/main/java/theCreepyProperty/main/Main.java
@@ -1,4 +1,4 @@
-package main;
+package theCreepyProperty.main;
 
 public class Main {
     public static void main(String[] args) throws Exception {
